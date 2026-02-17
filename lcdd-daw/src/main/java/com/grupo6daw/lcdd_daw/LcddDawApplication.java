1d6a49090ad9bedc966bfa8f7e778b930adbabb1
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2026-02-17 09:12:21 +0100

    spring config

---
+++ lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/LcddDawApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6daw.lcdd_daw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcddDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcddDawApplication.class, args);
+	}
+
+}
