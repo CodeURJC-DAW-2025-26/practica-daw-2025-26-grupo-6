ad6c648a20ca2337ee6be613e5e93ce280927335
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2026-02-17 19:03:40 +0100

    ImageController, ImageRepository, ImageService and class

---
+++ lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.grupo6daw.lcdd_daw.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.grupo6daw.lcdd_daw.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
