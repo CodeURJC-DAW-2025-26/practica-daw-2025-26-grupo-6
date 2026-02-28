a30c8a61d70583b922f5be41e6f4e7eec852712a
Scabfull <46084912+Scabfull@users.noreply.github.com>
2026-02-28 16:17:28 +0100

    Login validation changed

--- lcdd-daw/src/main/resources/static/js/main.js
+++ lcdd-daw/src/main/resources/static/js/main.js
@@ -212,3 +212,22 @@
   }
 
 })();
+
+(function () {
+  'use strict'
+
+  // Search for all forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // boolean checkValidity() method of HTMLFormElement to prevent submission if there are invalid fields
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
