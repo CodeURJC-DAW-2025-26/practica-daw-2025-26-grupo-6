a8623480eaecc40de42b15d75dfbd1bd2c929ced
fishmanaym <69584761+fishmanaym@users.noreply.github.com>
2026-02-28 03:36:09 +0100

    user profile display

--- lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/controller/UserController.java
+++ lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/controller/UserController.java
@@ -1,13 +1,48 @@
 package com.grupo6daw.lcdd_daw.controller;
 
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.grupo6daw.lcdd_daw.model.User;
+import com.grupo6daw.lcdd_daw.service.UserService;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 
 @Controller
 public class UserController {
 	
+	@Autowired
+	UserService userService;
+
 	@GetMapping("/profile")
-	public String profile() {
+	public String profile(Model model) {
+		Long id = Long.parseLong((String) model.getAttribute("userId"));
+		User user = userService.getUser(id).orElseThrow();
+		model.addAttribute("user", user);
 		return "profile";
+	}
+
+	// Get image for users, if they have no image it returns the default image
+	@GetMapping("/user/{id}/image")
+	public ResponseEntity<Resource> getUserImage(@PathVariable long id) throws SQLException {
+
+		Resource imageFile = userService.getImageFile(id);
+
+		MediaType mediaType = MediaTypeFactory
+			.getMediaType(imageFile)
+			.orElse(MediaType.IMAGE_JPEG);
+
+		return ResponseEntity
+			.ok()
+			.contentType(mediaType)
+			.body(imageFile);
 	}
 }
--- lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/repository/UserRepository.java
+++ lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/repository/UserRepository.java
@@ -3,12 +3,16 @@ package com.grupo6daw.lcdd_daw.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.grupo6daw.lcdd_daw.model.Image;
 import com.grupo6daw.lcdd_daw.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserEmail(String email);
 
     boolean existsByUserEmail(String email);
-}
 
+    @Query("SELECT u.userImage from UserTable u WHERE u.userId = :id")
+    Optional<Image> findImageByUserId(long id);
+}
