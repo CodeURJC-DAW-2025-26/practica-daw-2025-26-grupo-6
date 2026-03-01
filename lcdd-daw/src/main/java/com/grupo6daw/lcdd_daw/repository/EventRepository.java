6ee063a3df4a894d7d5a7e985d2290fba1059f92
Scabfull <46084912+Scabfull@users.noreply.github.com>
2026-03-01 15:03:03 +0100

    update event description styling to improve text wrapping in events template

--- lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/repository/EventRepository.java
+++ lcdd-daw/src/main/java/com/grupo6daw/lcdd_daw/repository/EventRepository.java
@@ -13,41 +13,39 @@ import com.grupo6daw.lcdd_daw.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    @Query("""
-                SELECT e AS event, COUNT(DISTINCT u) AS participantCount
-                FROM Event e
-                LEFT JOIN e.eventRegisteredUsers u
-                GROUP BY e
-                ORDER BY COUNT(DISTINCT u) DESC, e.eventName ASC
-            """)
-    Page<Object[]> findEventsOrderedByParticipants(Pageable pageable);
+        @Query("""
+                            SELECT e AS event, COUNT(DISTINCT u) AS participantCount
+                            FROM Event e
+                            LEFT JOIN e.eventRegisteredUsers u
+                            GROUP BY e
+                            ORDER BY COUNT(DISTINCT u) DESC, e.eventName ASC
+                        """)
+        Page<Object[]> findEventsOrderedByParticipants(Pageable pageable);
 
-    Optional<Event> findByEventName(String eventName);
+        Optional<Event> findByEventName(String eventName);
 
-    @Query("""
-            SELECT e
-            FROM Event e
-            WHERE
-            (LOWER(e.eventName) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL OR :name = '') AND
-            (LOWER(e.eventTag) = LOWER(:tag) OR :tag IS NULL OR :tag = '')
-            """)
-    List<Event> findByNameAndTag(String name, String tag);
+        @Query("""
+                        SELECT e
+                        FROM Event e
+                        WHERE
+                        (LOWER(e.eventName) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL OR :name = '') AND
+                        (LOWER(e.eventTag) = LOWER(:tag) OR :tag IS NULL OR :tag = '')
+                        """)
+        List<Event> findByNameAndTag(String name, String tag);
 
-    List<Event> findByValidatedTrue();
+        List<Event> findByValidatedTrue();
 
-    List<Event> findByValidatedFalse();
+        List<Event> findByValidatedFalse();
 
-   
-    List<Event> findByEventDateGreaterThanEqualOrderByEventDateAsc(LocalDate date);
+        List<Event> findByEventDateGreaterThanEqualOrderByEventDateAsc(LocalDate date);
 
-    @Query("""
-             SELECT e
-             FROM Event e
-             WHERE
-               e.validated = true
-               AND (LOWER(e.eventName) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL OR :name = '')
-               AND (LOWER(e.eventTag) = LOWER(:tag) OR :tag IS NULL OR :tag = '')
-             ORDER BY e.eventDate DESC
-            """)
-    Page<Event> findValidatedByNameAndTag(String name, String tag, Pageable page);
+        @Query("""
+                         SELECT e
+                         FROM Event e
+                         WHERE
+                           e.validated = true
+                           AND (LOWER(e.eventName) LIKE LOWER(CONCAT('%', :name, '%')) OR :name IS NULL OR :name = '')
+                           AND (LOWER(e.eventTag) = LOWER(:tag) OR :tag IS NULL OR :tag = '')
+                        """)
+        Page<Event> findValidatedByNameAndTag(String name, String tag, Pageable page);
 }
